9509507cfac9daf151c96705198f566c0dacd2f0
Doug Stevenson <dougstevenson@google.com>
2016-11-30 09:19:58 -0800

    Basic auth integration

--- app/src/main/java/com/adclivecode/MainActivity.java
+++ app/src/main/java/com/adclivecode/MainActivity.java
@@ -1,19 +1,47 @@
 package com.adclivecode;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.w3c.dom.Text;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 1;
+    private static final String LOG_TAG = "MainActivity";
+
+    private TextView tvHello;
+
+    private final FirebaseAuth auth = FirebaseAuth.getInstance();
+    private FirebaseAuth.AuthStateListener authStateListener = new FirebaseAuth.AuthStateListener() {
+        @Override
+        public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+            updateViews();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        initViews();
+    }
+
+    private void initViews() {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -26,6 +54,19 @@ public class MainActivity extends AppCompatActivity {
                     .setAction("Action", null).show();
             }
         });
+
+        tvHello = (TextView) findViewById(R.id.tv_hello);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        auth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
     }
 
     @Override
@@ -42,11 +83,36 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id) {
+            case R.id.action_login:
+                Log.d(LOG_TAG, "login");
+                startActivityForResult(
+                    AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setProviders(Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
+                        .build(),
+                    RC_SIGN_IN);
+                return true;
+            case R.id.action_logout:
+                auth.signOut();
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+    private void updateViews() {
+        final FirebaseUser user = auth.getCurrentUser();
+        if (user != null) {
+            String name = user.getDisplayName();
+            if (name == null) {
+                name = "unnamed user";
+            }
+            tvHello.setText(name);
+        }
+        else {
+            tvHello.setText("You should log in!");
+        }
+    }
+
 }
